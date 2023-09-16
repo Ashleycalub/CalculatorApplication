a5a2ca7484340137390b8c74f17fa41e6c8acaf1
reyes.275973 <reyes.275973@LAB-B-PC08>
2023-09-16 16:50:53 +0800

    Add project files.

---
+++ CalculatorApplication/Form1.cs
@@ -0,0 +1,15 @@
+namespace CalculatorApplication
+{
+    public partial class FrmCalculator : Form
+    {
+        public FrmCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
