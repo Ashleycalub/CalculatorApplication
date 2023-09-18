441e0c693faae21be1f794d766077c40d055ec43
Ashleycalub <Acreyes980@gmail.com>
2023-09-18 10:12:39 +0800

    CalculatorApplication

---
+++ CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    public delegate T Formula<T>(T arg, T arg1);
+
+    internal class CalculatorClass
+    {
+        public Formula<double> total;
+        public double GetSum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double GetDifference(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public event Formula<double> CalculateEvent
+        {
+            add
+            {
+                Console.WriteLine("Added the delegate");
+                total += value;
+            }
+            remove
+            {
+                Console.WriteLine("Remove the delegeate");
+                total -= value;
+            }
+        }
+        public double GetProduct(double num1, double num2)
+        {
+            return num1 * num2;
+
+        }
+        public double GetQoutient(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
